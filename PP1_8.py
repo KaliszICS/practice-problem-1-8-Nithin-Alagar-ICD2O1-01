36f680c7f62bfd1061e32f510563b169a41b3301
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-11-04 17:19:03 -0500

    nithin

--- PP1_8.py
+++ PP1_8.py
@@ -21,7 +21,7 @@ def q4():
   #Write Assignment code here
   food = input("Input food: ")
   drink = input("Input drink: ")
-  result = food == "pizza" and drink == "pop"
+  result = not food == "pizza" and not drink == "pop"
   print(result)
 def q5():
   #Write Assignment code here
