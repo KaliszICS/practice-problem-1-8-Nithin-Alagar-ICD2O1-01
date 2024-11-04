e5798e129e1f26915baa7cf42b8f5084e81e3c9a
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-11-04 17:12:39 -0500

    nithin

--- PP1_8.py
+++ PP1_8.py
@@ -1,19 +1,36 @@
 
 def q1():
   #Write Assignment code here
-
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+  print(bool1 or bool2)
 def q2():
   #Write Assignment code here
-
+  num = input("Enter an integer: ")
+  num = int(num)
+  bool = 0 == value
+  print(bool)
 def q3():
   #Write Assignment code here
-
+  num3 = input("Enter a number: ")
+  num3 = int(num3)
+  value2 = num3 >= 0 and num3 <= 10
+  print(value2)
 def q4():
   #Write Assignment code here
-
+  food = input("Input food: ")
+  drink = input("Input drink: ")
+  result = food == "pizza" and drink == "pop"
+  print(result)
 def q5():
   #Write Assignment code here
-
+  numb = input("Enter an integer: ")
+  numb = int(numb)
+  if  numb%2 = 0:
+    print(f"The integer {numb} is True."
+  else:
+      print(f"The integer {numb} is False.")
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
